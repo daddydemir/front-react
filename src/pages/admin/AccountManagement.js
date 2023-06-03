6bba33c5890c1a97b268cafb93767d9381753e30
daddydemir <42716195+daddydemir@users.noreply.github.com>
2023-06-03 22:24:42 +0300

    account page is started.

--- src/pages/admin/AccountManagement.js
+++ src/pages/admin/AccountManagement.js
@@ -1,9 +1,44 @@
+import {Col, Row} from "react-bootstrap";
 
 export const AccountManagement = () => {
 
+    const handler = () => {
+
+    }
     return (
-        <div>
-            Welcome to AccountManagement page
+        <div className='admin-default'>
+            <h3 className='header'> ACCOUNT </h3>
+            <hr/>
+            <section>
+                <Row>
+                    <Col sm={6}>
+                        <img src='https://avatars.githubusercontent.com/u/42716195?v=4' className='pp-image'/>
+                        <h5> Mehmet Çakmaktaşı </h5>
+                    </Col>
+                    <Col sm={6}>
+                        <section className='form-style'>
+                            <form>
+                                <h4> Password Update </h4>
+                                <Row>
+                                    <Col sm={12}>
+                                        <input type='password' placeholder='old password'/>
+                                    </Col>
+                                    <Col sm={12}>
+                                        <input type='password' placeholder='new password'/>
+                                    </Col>
+                                    <Col sm={12}>
+                                        <input type='password' placeholder='confirm'/>
+                                    </Col>
+                                    <Col size={12} className='px-1'>
+                                        <button type='button' onClick={handler}><span>Save</span></button>
+                                    </Col>
+                                </Row>
+                            </form>
+                        </section>
+                    </Col>
+                </Row>
+
+            </section>
         </div>
     );
 }
